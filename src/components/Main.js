ac547c7d64e94996a4a5426cf8bfdb6ed5b3660d
Yan Pustynnyy <necrosith@yandex.ru>
2018-07-08 15:01:50 +0400

    Slider added

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
 
+import Slider from './Slider.js';
+
 const Repair = styled.div`
 	color: #ffffff;
 	font-size: 40px;
@@ -50,7 +52,8 @@ class Main extends React.Component {
 					<CallButton> Отправить заявку!
 					</CallButton>
 				</Col>
-				<Col lg={6} lfOffset={1}>
+				<Col lg={6} lgOffset={1}>
+					<Slider />
 				</Col>
 			</Row>
 		)
